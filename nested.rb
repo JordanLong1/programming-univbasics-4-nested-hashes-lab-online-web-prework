377c4590f270feb785cf89d2b8339c4011b38066
Jordan Long <jordanleelong5@gmail.com>
2019-09-26 00:59:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,11 +86,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 new_hash = :yukihiro_matsumoto {
-			 :known_for => "Ruby",
-			 :languages => ["LISP", "C"]
-		 }
-puts new_hash
+		
 
 end
 
